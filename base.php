fc992c654cbd4f2dc54bd35ee65d0a4fe54aa426
devDaniel0330 <danthecoder77@gmail.com>
2024-12-04 11:05:31 +0800

    added database setup, general login logout functions, and general redirecting function

--- base.php
+++ base.php
@@ -13,17 +13,74 @@ function is_post() {
     return $_SERVER['REQUEST_METHOD'] == 'POST';
 }
 
+// GET parameter
 function get($key, $value = null) {
     $value = $_GET[$key] ?? $value;
     return is_array($value) ? array_map('trim', $value) : trim($value);
 }
 
+// POST parameter
 function post($key, $value = null) {
     $value = $_POST[$key] ?? $value;
     return is_array($value) ? array_map('trim', $value) : trim($value);
 }
 
+// GET and POST parameter (REQUEST)
 function req($key, $value = null) {
     $value = $_REQUEST[$key] ?? $value;
     return is_array($value) ? array_map('trim', $value) : trim($value);
 }
+
+// Redirecting URL
+function redirect($url = null) {
+    $url ??= $_SERVER['REQUEST_URI'];
+    header("Location: $url");
+    exit();
+}
+
+// set/get temporary session variable
+function temp($key, $value = null) {
+    if ($value !== null) {
+        $_SESSION["temp_$key"] = $value;
+    } else {
+        $value = $_SESSION["temp_$key"] ?? null;
+        unset($_SESSION["temp_$key"]);
+        return $value;
+    }
+}
+
+// Security
+
+$_user = $_SESSION['user'] ?? null;
+
+// User login
+function login($user, $url = '/') {
+    $_SESSION['user'] = $user;
+    redirect($url);
+}
+
+// User logout
+function logout($url = '/') {
+    unset($_SESSION['user']);
+    redirect($url);
+}
+
+// Authorization
+function auth(...$roles) {
+    global $_user;
+    if ($_user) {
+        if ($roles) {
+            if (in_array($_user->role, $roles)) {
+                return;
+            }
+        } else {
+            return;
+        }
+    }
+    redirect('/login.php');
+}
+
+// Database setup
+
+// Global PDO Object
+$_db = new PDO('mysql:dbname=booksdb', 'root', '', [PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,]);
